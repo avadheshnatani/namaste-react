11e31bc035fd83ebb330669dc8a44b9369b7ab97
nataniavadhesh <avadhesh.natani@external.danone.com>
2024-04-10 12:29:14 +0530

    Browser list config

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 
 const parent = React.createElement('div', {id:'parent'}, [
     React.createElement('div', {id:'child1'}, [
-        React.createElement('h1', {id:'heading1'}, 'Heading 1'),
+        React.createElement('h1', {id:'heading1', className:'heading'}, 'Heading 1'),
         React.createElement('h2', {id:'heading2'}, 'Heading 2')
     ]),
     React.createElement('div', {id:'child2'}, [
